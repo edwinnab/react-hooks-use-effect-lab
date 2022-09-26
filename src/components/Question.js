24a82b86837dccd57716e8db01c33ad06786dd66
edwinnab <edwinnabikeri@gmail.com>
2022-09-26 13:30:15 +0300

    useEffect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
+//question component is taking the question and onAnswered props from app
 function Question({ question, onAnswered }) {
+  //this is the state
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    if(timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return; //exits early
+    }
+
+    const timerId = setTimeout( () => { setTimeRemaining(timeRemaining -1)}, 
+    1000);
+
+    return function() {
+      clearTimeout(timerId)
+    };
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
